21abe9e2b120294043f3f4dc665bcfedebf5177e
SLMarble <126801080+SLMarble@users.noreply.github.com>
2024-05-15 16:25:45 +0800

    Update permission.js

--- src/permission.js
+++ src/permission.js
@@ -22,7 +22,7 @@ router.beforeEach(async(to, from, next) => {
   if (hasToken) {
     if (to.path === '/client/login') {
       // if is logged in, redirect to the home page
-      next({ path: '/' })
+      next()
     } else {
       next()
     }
