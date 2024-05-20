17dcd3323e305bc41272c3a4397990b2679fce2a
SakuraLong <1897111@qq.com>
2024-05-20 21:47:43 +0800

    更新设计流程图页面

---
+++ src/views/GraphShower/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="graph-shower-page">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.graph-shower-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 99999;
+  overflow: auto;
+  background-color: white;
+}
+.graph-shower-page > iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
